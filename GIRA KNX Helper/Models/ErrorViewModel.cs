f206a7f77c4bf57ce780aec63319ff8692f63442
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-09-20 17:18:40 +0300

    Add project files.

---
+++ GIRA KNX Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GIRA_KNX_Helper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
