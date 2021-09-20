95c635eeae47bdce577ab08d4a57b738ba7c56a8
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-09-20 17:54:44 +0300

    feat : add GAEdit view

---
+++ GIRA KNX Helper/Models/VM/GAVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GIRA_KNX_Helper.Models.VM
+{
+    public class GAVM
+    {
+        public string Address { get; set; }
+    }
+}
